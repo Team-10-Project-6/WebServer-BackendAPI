704030f91d0b58ac20d8216da03b82505a5948ac
abdyh <aali3585@conestogac.on.ca>
2026-02-09 14:36:54 -0500

    #5: Moved GET logic to a separate function to bypass the @require_auth decorator, so GET /api/posts requests should be availabe to the public.

--- app/routes/posts.py
+++ app/routes/posts.py
@@ -8,45 +8,45 @@ import base64
 
 bp = Blueprint('posts', __name__)
 
-@bp.route('/posts', methods=['GET', 'POST'])
-@require_auth
-def manage_posts():
-    if request.method == 'POST':
-        user_id = get_or_create_user(g.user_claims['sub'])
-        data = request.json
-        
-        if not data.get('image'):
-            return jsonify({"error": "No image data provided"}), 400
-        
-        # Decode base64 image
-        try:
-            image_blob = base64.b64decode(data['image'])
-        except Exception as e:
-            return jsonify({"error": "Invalid image data", "details": str(e)}), 400
-        
-        description = data.get("description", "").strip()
-        filename = data.get("filename", "image.jpg")
-
-        create_post(user_id, filename, description, image_blob)
-        
-        return jsonify({"message": "Post created successfully"}), 201
-
-    # GET: Retrieve all posts
-    post_rows = get_all_posts()
-    
+@bp.route('/posts', methods=['GET'])
+def list_posts():
+    # GET: Retrieve all posts and comments
+    posts = get_all_posts()
     results = []
-    for post in post_rows:
-        comment_rows = get_comments_for_post(post["id"])
-        
+    for post in posts:
+        comments = get_comments_for_post(post["id"])
         results.append({
             "id": post["id"],
             "description": post["description"],
             "username": post["username"],
-            "comments": [{"text": c["comment_text"], "author": c["username"]} for c in comment_rows],
+            "uploaded_at": post["uploaded_at"],
+            "comments": [{"text": c["comment_text"], "author": c["username"]} for c in comments],
             "image_blob": base64.b64encode(post["data"]).decode('utf-8')  # Encode image blob as base64 string for frontend
         })
+        return jsonify(results)
+
+@bp.route('/posts', methods=['POST'])
+@require_auth
+def create_post():
+    user_id = get_or_create_user(g.user_claims['sub'])
+    data = request.json
     
-    return jsonify(results)
+    if not data.get('image'):
+        return jsonify({"error": "No image data provided"}), 400
+    
+    # Decode base64 image
+    try:
+        image_blob = base64.b64decode(data['image'])
+    except Exception as e:
+        return jsonify({"error": "Invalid image data", "details": str(e)}), 400
+    
+    description = data.get("description", "").strip()
+    filename = data.get("filename", "image.jpg")
+
+    create_post(user_id, filename, description, image_blob)
+    
+    return jsonify({"message": "Post created successfully"}), 201
+
 
 @bp.route('/images/download/<int:post_id>')
 def serve_blob(post_id):
